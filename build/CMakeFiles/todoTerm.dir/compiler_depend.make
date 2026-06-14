238f63ac37a30e124858205eba36b6233f4cadf6
Shreyo Pal <scripterzofindia@gmail.com>
2026-06-14 11:29:19 +0000

    Setup Cmake

---
+++ build/CMakeFiles/todoTerm.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for todoTerm.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/todoTerm.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/todoTerm.dir/src/main.cpp.o: \
+ /mnt/c/Users/Shreyo\ Pal/Documents/Coding/C++/todo-app/src/main.cpp \
+ /usr/include/stdc-predef.h
